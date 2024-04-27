731280a9a0aa1f9b5665b7dc09f716cb7676d921
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-04-27 23:02:57 +0600

    data stored to the database

--- index.js
+++ index.js
@@ -26,13 +26,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+  
+    const materialCollection = client.db('materialDB').collection('material')
+  
+    app.post('/data', async(req, res)=>{
+      const newMaterial = req.body
+      console.log(newMaterial);
+      const result = await materialCollection.insertOne(newMaterial)
+      res.send(result);
+    })
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
